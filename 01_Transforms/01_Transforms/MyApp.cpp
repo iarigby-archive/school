a649e5bfc8314cf601751825698ba2de9aac7f87
Ia Rigby <marvinzem@gmail.com>
2018-11-06 16:41:30 +0100

    rotate around z axis

--- 01_Transforms/01_Transforms/MyApp.cpp
+++ 01_Transforms/01_Transforms/MyApp.cpp
@@ -153,7 +153,7 @@ void CMyApp::Render()
 	*/
 	// <float> is needed because vec  is int and pi is float and results in error
 	int t = SDL_GetTicks();
-	m_matWorld = glm::rotate<float>(2*M_PI *t/ 1000.0f, glm::vec3(1, 0, 0));
+	m_matWorld = glm::rotate<float>(2*M_PI *t/ 1000.0f, glm::vec3(0, 0, 1));
 
 	glUniformMatrix4fv( m_loc_world,// uniform's location
 						1,			// send 1 matrix
